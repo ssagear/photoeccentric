faef40be0b21493ab88e1c6c8604dff40e69d0c0
Sheila Sagear <ssagear@ufl.edu>
2022-06-24 10:02:45 -0700

    using virtualev for rtd build

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
 from setuptools import setup
-setup(use_scm_version=True) 
+#setup(use_scm_version=True)
+
+setup(name='photoeccentric',
+      version='0.1',
+      description="Photoeccentric",
+      long_description="",
+      author='Sheila Sagear',
+      author_email='ssagear@ufl.edu',
+      license='MIT',
+      packages=['photoeccentric'],
+      zip_safe=False,
+      # install_requires=[
+      #     'Django',
+      #     # 'Sphinx',
+      #     # ^^^ Not sure if this is needed on readthedocs.org
+      #     # 'something else?',
+      #     ],
+      )
